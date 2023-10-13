62b557558cf521e4f8e84acf3bf444e42f239164
jlou0008 <jlou0008@student.monash.edu>
2023-10-13 14:46:41 +1100

    emailwithattachments

---
+++ FIT5032_MyProject/Models/SendReportViewModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace FIT5032_MyProject.Models
+{
+    public class SendReportViewModels
+    {
+        public int ReportId { get; set; } // To track which report is being sent
+
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "Please enter an email address.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string ToEmail { get; set; }
+
+        // You can add more fields as needed (e.g., Subject, Body, etc.) but for now I've just kept the recipient email.
+    }
+}
---
+++ FIT5032_MyProject/Utils/EmailSender.cs
@@ -0,0 +1,43 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.IO;
+using System.Net.Mail;
+
+namespace FIT5032_MyProject.Utils
+{
+    public class EmailSender
+    {
+        // Please use your SendGrid API KEY here.
+        // FIT5032PROJECT API SENDGIRD
+        private const String API_KEY = "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";
+
+        public void Send(String toEmail, String subject, String contents)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("jlou0008@student.monash.edu", "FIT5032 Notification");
+            var to = new EmailAddress(toEmail, "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+
+            var response = client.SendEmailAsync(msg);
+        }
+
+        public void SendWithAttachment(String toEmail, String subject, String contents, String attachmentPath)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("jlou0008@student.monash.edu", "FIT5032 Notification");
+            var to = new EmailAddress(toEmail, "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+
+            var bytes = File.ReadAllBytes(attachmentPath);
+            var file = Convert.ToBase64String(bytes);
+            msg.AddAttachment(Path.GetFileName(attachmentPath), file);
+
+            var response = client.SendEmailAsync(msg);
+        }
+    }
+}
