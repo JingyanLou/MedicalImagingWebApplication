a21a971fa621cfeb13d0c8513f1e983821d938b7
jlou0008 <jlou0008@student.monash.edu>
2023-10-18 14:03:07 +1100

    interactive datatable

---
+++ FIT5032_MyProject/Controllers/StatsController.cs
@@ -0,0 +1,50 @@
+﻿using FIT5032_MyProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace FIT5032_MyProject.Controllers
+{
+    public class StatsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Stats
+        public ActionResult BookingStats()
+        {
+            StatsViewModel model = new StatsViewModel();
+
+            return View(model);
+        }
+        
+        [HttpPost]
+        public ActionResult BookingStats(DateTime? startDate, DateTime? endDate)
+        {
+            DateTime start = startDate ?? DateTime.Now.AddDays(-7);
+            DateTime end = endDate ?? DateTime.Now;
+
+            var stats = db.BookableTimeSlots
+                   .Where(b => b.Date >= start && b.Date <= end)
+                   .GroupBy(b => b.BranchId)
+                   .Select(g => new { BranchId = g.Key, Count = g.Count() })
+                   .ToList();
+
+            var viewModel = new StatsViewModel
+            {
+                StartDate = start,
+                EndDate = end,
+                BranchNames = new List<string>(),
+                BookingCounts = new List<int>()
+            };
+
+            foreach (var stat in stats)
+            {
+                viewModel.BranchNames.Add(db.Branches.Find(stat.BranchId).Name);
+                viewModel.BookingCounts.Add(stat.Count);
+            }
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ FIT5032_MyProject/Models/StatsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_MyProject.Models
+{
+    public class StatsViewModel
+    {
+        public List<string> BranchNames { get; set; }
+        public List<int> BookingCounts { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
+
